4085a5dd7f8ddc97a29e5b64d62e7c1d45951288
hyginn <boris.steipe@utoronto.ca>
2017-01-19 17:11:39 -0500

    Update

--- sampleSolutions.R
+++ sampleSolutions.R
@@ -104,5 +104,57 @@ summary(x)
 
 
 
+# ==============================================================================
+#      PART TWO: TASKS FOR 02_Features
+# ==============================================================================
+
+
+#   - Write code to plot the genes with the five highest and lowest loadings
+#     of PC1, highest in black, and lowest in red.
+
+# get the five highest values from pcaYG$x[ ,1] ...
+sel1 <- order(pcaYG$x[ ,2], decreasing = TRUE)[1:5]
+# and the five lowest values from pcaYG$x[ ,1] ...
+sel2 <- order(pcaYG$x[ ,2], decreasing = FALSE)[1:5]
+
+plot(seq(0, 120, by = 5), rep(0, 25), type = "n",
+     ylim = c(-1.5, 1.5),
+     xlab = "time", ylab = "log-ratio expression")
+rect( 22.5, -2,  37.5, 2, col = "#dfeaf4", border = NA)   # G0
+rect( 82.5, -2,  97.5, 2, col = "#dfeaf4", border = NA)   # G0
+rect( 52.5, -2,  67.5, 2, col = "#f4dfdf", border = NA)   # G1
+rect(112.5, -2, 122.5, 2, col = "#f4dfdf", border = NA)   # G1
+
+for (i in 1:5) {
+    points(seq(0, 120, by = 5), ygProfiles[sel1[i], ], type = "b", col = "black")
+    points(seq(0, 120, by = 5), ygProfiles[sel2[i], ], type = "b", col = "red")
+}
+
+# ==============================================================================
+#      PART THREE: TASKS FOR 03_Models
+# ==============================================================================
+
+# Review: Curve fit of gene expression: measuring GFP fluorescence in one-minute
+# intervals yields the following data.
+myDat <- data.frame( fl = c(
+    710.1697,  689.2170,  714.3733,  688.9666,  653.9151,  628.6372,
+    600.6179,  558.6891,  522.2780,  507.8960,  573.6212,  539.6243,
+    525.4207,  539.3329,  524.4395,  501.4929,  492.4278,  480.4025,
+    511.6782,  458.4573,  445.0892,  443.1762,  387.9263,  412.9964,
+    392.0517,  364.3173,  371.6392,  392.1216,  364.8162,  375.5662,
+    379.2305,  341.3690,  285.1799,  309.8994,  303.8815,  334.6760,
+    342.3248,  283.7887,  272.3799,  234.8944,  294.6621,  278.1166,
+    297.8715,  238.9789,  262.9021,  216.6459,  245.6330,  235.6556,
+    241.1547,  267.7355,  200.9985,  173.5843,  223.2996,  181.2174,
+    191.2544,  175.2843,  207.8503,  177.1721,  187.3568,  190.0217,
+    121.8007,  136.6225,  144.2138,  190.5096,  150.6899,  145.2135,
+    124.6761,  115.2702,  138.4352,  96.1188,   107.3551,  103.1700,
+    141.4267,  103.9856,  137.1076,  88.6598,   106.0739),
+    t = 1:77)
+
+# How can we model this?
+# What are the relevant parameters?
+
+
 
 # [END]
